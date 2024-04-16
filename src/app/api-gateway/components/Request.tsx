7d7ece0db2f70aa6766c9a4b2ed59089126287a0
Ryan Punwasi <ryan.punwasi@gmail.com>
2024-04-15 23:41:23 -0400

    Render fetched data

--- src/app/api-gateway/components/Request.tsx
+++ src/app/api-gateway/components/Request.tsx
@@ -10,7 +10,9 @@ export const Request = () => {
 
     try {
       await new Promise(resolve => setTimeout(resolve, 2000));
-      const response = await fetch("/api/data");
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/todos/1"
+      );
       const json = await response.json();
       setData(json);
       setStatus("idle");
@@ -25,13 +27,13 @@ export const Request = () => {
       {status === "idle" && (
         <button
           onClick={sendRequest}
-          className="w-3/4 rounded-lg dark:bg-indigo-600 text-white text-sm filter saturate-200 px-3 py-2 dark:hover:bg-indigo-500 dark:active:bg-indigo-700"
+          className="w-48 rounded-lg dark:bg-indigo-600 text-white text-sm filter saturate-200 px-3 py-2 dark:hover:bg-indigo-500 dark:active:bg-indigo-700"
         >
           Send Request
         </button>
       )}
       {status === "pending" && (
-        <button className="w-3/4 rounded-lg dark:bg-indigo-600 text-white text-sm filter saturate-200 px-3 py-2 dark:hover:bg-indigo-500 dark:active:bg-indigo-700 flex gap-2 items-center justify-center">
+        <button className="w-48 rounded-lg dark:bg-indigo-600 text-white text-sm filter saturate-200 px-3 py-2 dark:hover:bg-indigo-500 dark:active:bg-indigo-700 flex gap-2 items-center justify-center">
           <div role="status">
             <svg
               aria-hidden="true"
@@ -63,13 +65,18 @@ export const Request = () => {
         </button>
       )}
       {data === null && status !== "pending" && (
-        <section className="dark:bg-gray-800 rounded-lg p-10 text-sm font-semibold border-4 border-dashed border-gray-700 dark:text-gray-400">
+        <section className="w-96 flex items-center justify-center dark:bg-gray-800 rounded-lg p-10 text-sm font-semibold border-4 border-dashed border-gray-700 dark:text-gray-400">
           Data will show up here.
         </section>
       )}
       {status === "pending" && (
-        <section className="animate-pulse dark:bg-gray-800 rounded-lg p-10 text-sm font-semibold border-4 border-dashed border-gray-700 dark:text-gray-400">
+        <section className="w-96 animate-pulse flex items-center justify-center dark:bg-gray-800 rounded-lg p-10 text-sm font-semibold border-4 border-dashed border-gray-700 dark:text-gray-400">
           Data will show up here.
+        </section>
+      )}
+      {data != null && status !== "pending" && (
+        <section className="w-96 dark:bg-gray-800 flex items-center justify-center rounded-lg p-10 text-sm font-semibold dark:text-gray-400">
+          {JSON.stringify(data, null, 2)}
         </section>
       )}
     </div>
